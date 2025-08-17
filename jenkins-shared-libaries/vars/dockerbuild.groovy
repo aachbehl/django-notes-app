926fd52bb4bdfeb423f91b2fbe5a023402fe1464
aachbehl <aachalbehl24@gmail.com>
2025-08-17 20:06:15 +0530

    Delete jenkins-shared-libaries/vars directory

--- jenkins-shared-libaries/vars/dockerbuild.groovy
+++
@@ -1,3 +0,0 @@
-def call(String Projectname,String Imagetag,String dockerHubUser){
-  sh "docker build -t {$dockerHubUser}/${Projectname}:${Imagetag} ."
-}
--- jenkins-shared-libaries/vars/gitclone.groovy
+++
@@ -1,5 +0,0 @@
-def call(String url,String branch){
-  echo "This is copying the code"
-  git url:"${url}",branch:"${branch}
-  echo "Code clones successfully"
-}
